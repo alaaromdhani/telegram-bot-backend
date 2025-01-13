67592dd136052c20c1554eb73f476f0a7fefd726
ala.romdhani@esprit.tn <ala.romdhani@esprit.tn>
2025-01-13 22:21:55 +0000

    first-commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Import cors middleware
+const app = express()
+const port = 3000
+app.use(cors({
+    origin:'http://localhost:5500'
+}));
+app.get('/', (req, res) => {
+  res.send({
+    response:'ok'
+  })
+})
+
+
+
+// Middleware to handle JSON requests
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
