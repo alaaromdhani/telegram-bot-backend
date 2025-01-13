e2a68c6e04d742dc62a16eefd719d0feeeba6558
ala.romdhani@esprit.tn <ala.romdhani@esprit.tn>
2025-01-13 23:02:35 +0000

    changing-port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require("body-parser");
 const cors = require("cors"); // Import cors middleware
 const app = express()
-const port = 3000
+const port = 80
 app.use(cors({
     origin:'http://localhost:5500'
 }));
